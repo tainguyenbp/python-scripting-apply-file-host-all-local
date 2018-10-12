3b57c6b2b9c8346887598fe71a5c3b6748daab27
nntai <nguyenngoctaibp@gmail.com>
2018-10-13 00:58:27 +0700

    Done logic script python3 apply-file-host.py

--- apply-file-host.py
+++ apply-file-host.py
@@ -3,26 +3,31 @@ import os
 import paramiko
 from scp import SCPClient
 
+def remove_row_empty_file_csv_use_bash():
 
-def ssh_command(ssh):
-    command = input("Command:")
-    ssh.invoke_shell()
-    stdin, stdout, stderr = ssh.exec_command(command)
-    print(stdout.read())
+    try:
+        cmd_get_pwd = os.getcwd()
+        cmd_remove_all_row_empty = ('sed -i /^$/d ' + cmd_get_pwd + '/server.csv')
+        os.system(cmd_remove_all_row_empty)
+
+    except Exception as e:
+        print('Command does not work !!! Please edit code')
+        print(e)
 
 def scp_file_to_client(ssh):
     try:
         with SCPClient(ssh.get_transport()) as scp:
             print('Copy file hosts to client')
-            scp.put('server.csv', '/home/server.csv')
+            scp.put('hosts', '/etc/hosts')
             scp.close()
             print('Done !!! copy file hosts to client')
 
     except Exception as e:
-            print('Establish connection SCP to client failed')
+            print('Establish connection SCP to client failed !!!')
             print(e)
 
 def ssh_connect_scp_file(host, port, user, password):
+
     try:
         ssh = paramiko.SSHClient()
         print('Start connect ssh to client')
@@ -32,7 +37,7 @@ def ssh_connect_scp_file(host, port, user, password):
         ssh.close()
 
     except Exception as e:
-        print('Establish connection SSH to client failed')
+        print('Establish connection SSH to client failed !!!')
         print(e)
 
 
@@ -41,6 +46,8 @@ if __name__=='__main__':
     if os.path.isfile('hosts'):
             if os.path.isfile('server.csv'):
 
+                remove_row_empty_file_csv_use_bash()
+
                 with open('server.csv','r') as open_file_csv:
 
                     read_file_csv = csv.reader(open_file_csv, delimiter=',')
@@ -48,8 +55,8 @@ if __name__=='__main__':
                     next(open_file_csv) # skip header
 
                     for row in read_file_csv:
+
                         print('===================================================================================')
-                        
                         host = row[0]
                         print('Ip Host:', host)
                         port = row[3]
@@ -60,8 +67,8 @@ if __name__=='__main__':
                         print('Password:',password)
 
                         ssh_connect_scp_file(host, port, user, password)
-
+                open_file_csv.close()
             else:
-                print ('File server.csv not exists')
+                print ('File server.csv not exists !!! ')
     else:
-        print('File hosts not exists')
+        print('File hosts not exists !!!')
